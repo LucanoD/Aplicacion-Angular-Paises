0622cf6a98a427109fa41f5333f33f3e8f93c152
Dani Lucano <Olucano10@gmail.com>
2021-11-12 02:18:08 -0500

    Manejo de errores

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,19 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { PaisService } from '../../services/pais.service'
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class PorPaisComponent {
-
   termino: string = '';
+  hayError: boolean = false;
 
-  constructor() { }
+  constructor(private paisService: PaisService) {}
 
-  buscar(){
-    console.log(this.termino);
+  buscar() {
+    this.hayError = false;
+    console.log(this.termino)
+
+    this.paisService.buscarPais(this.termino)
+    .subscribe((resp) => {
+      console.log(resp);
+    }, (err) =>{
+      this.hayError = true;
+    });
+
   }
-
 }
